3f4678ff07d334039f096d58a4aa4b1d6b61fd87
Nyholm <tobias.nyholm@gmail.com>
2013-04-04 10:51:30 +0200

    Added the instance name in the parameter

--- tomcat_deployment.pp
+++ tomcat_deployment.pp
@@ -7,25 +7,33 @@
 #		groupId => "se.company.application.service",
 #		artifactId => "my_bundle",
 #		version => "LATEST",
-#		warname => "great-service",
+#		intancename => "great-service",
 #	}
+# A war file will be place in /opt/data/instances/great-service/war/great-service.war
 #
 #
 # Repository might be: snapshots, testing or release etc
 # groupId is se.company.application.service
 # artifactId is the name of the artifact
 # $version may be a number, LATEST, RELEASE or "1.0-SNAPSHOT"
+# $instancename is the name of the folder that we put the war in
 # warname is the name of the link in topcat webapps folder, if excluded we use the artifactId (warname should not include ".war")
 # onlyon specifies a hostname that this artifact should be restricted to
-define tomcat::deployment($repository, $groupId, $artifactId, $version, $warname=false, $onlyon=false) {
+define tomcat::deployment($repository, $groupId, $artifactId, $version, $instancename=false, $warname=false, $onlyon=false) {
+	
+	#set the instancename to the artifact id if empty
+	if ( $instancename == false ){
+		$instancename = $artifactId
+	}
+	
+	#set the warname to the instancename if empty
+	if ( $warname == false ){
+		$warname = $instancename
+	}
 	
 	#set instance dir
 	$instancedir = "/opt/data/instances/$instancename"
 	
-	#set the warname to the artifact id if empty
-	if ( $warname == false ){
-		$warname = $artifactId
-	}
 
 	if ( $onlyon == $::hostname ) or ( $onlyon == false ){
 		#download form nexus
